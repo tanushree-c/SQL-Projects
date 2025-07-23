f4aa3666e212db52f31c3d09c69b5a9e3bebee22
Tanushree Chaturvedi <77103015+tanushree-c@users.noreply.github.com>
2025-07-24 01:55:54 +0530

    Update and rename Retails-Sales.sql to Retail-Sales-Analysis/Retails-Sales.sql

--- Retail-Sales-Analysis/Retails-Sales.sql
+++ Retail-Sales-Analysis/Retails-Sales.sql
@@ -1,6 +1,6 @@
 -- Create Database and Use It
-create database `retail sales analysis`;
-use `retail sales analysis`;
+CREATE database `retail sales analysis`;
+USE `retail sales analysis`;
 
 -- Create Table: retail_sales
 CREATE TABLE retail_sales (
@@ -25,17 +25,21 @@ SELECT
 FROM
     retail_sales;
 
+
 -- How many customers does the retailer have?
 SELECT 
     COUNT(DISTINCT customer_id) AS `Customers`
 FROM
     retail_sales;
 
+
 -- Which categories does the retailer have?
 SELECT DISTINCT
     category
 FROM
     retail_sales;
+
+
 
 -- Data Analysis & Business Key Problems and Answers
 
@@ -47,6 +51,7 @@ FROM
 WHERE
     sale_date = '2022-11-05';
 
+
 -- Q.2: Transactions from the 'Clothing' category with quantity >= 4 in November 2022
 SELECT 
     *
@@ -55,6 +60,7 @@ FROM
 WHERE
     category = 'Clothing' AND quantity >= 4
         AND DATE_FORMAT(sale_date, '%Y-%m') = '2022-11';
+
 
 -- Q.3: Total number of transactions and total sales per category
 SELECT 
@@ -65,6 +71,7 @@ FROM
     retail_sales
 GROUP BY category;
 
+
 -- Q.4: Average age of customers who purchased items from the 'Beauty' category
 SELECT 
     ROUND(AVG(age), 0) AS `Average Age`,
@@ -74,6 +81,7 @@ FROM
 WHERE
     category = 'Beauty';
 
+
 -- Q.5: Transactions where the total sale value exceeds 1000
 SELECT 
     *
@@ -82,14 +90,16 @@ FROM
 WHERE
     total_sale > 1000;
 
+
 -- Q.6: Total number of transactions by gender and category
 SELECT 
     gender, category, COUNT(transactions_id) AS Transactions
 FROM
     retail_sales
-GROUP BY gender , category;
+GROUP BY gender, category;
 
--- Q.7: Find the best selling month of each year based on highest average total sale
+
+-- Q.7: Find the best-selling month of each year based on the highest average total sales
 SELECT 
     Avg_Sale_table1.sale_year as Year,
     Avg_Sale_table1.Sale_month as `Best Selling Month`,
@@ -117,6 +127,7 @@ FROM
         AND Avg_Sale_Table1.Avg_sale = yearly_max_avg.max_avg_amt
 ORDER BY Avg_Sale_Table1.sale_year;
 
+
 -- Q.8: Top 5 customers based on highest total sales
 SELECT 
     customer_id, SUM(total_sale) AS total_sale
@@ -126,6 +137,7 @@ GROUP BY customer_id
 ORDER BY total_sale DESC
 LIMIT 5;
 
+
 -- Q.9: Number of unique customers who purchased items from each category
 SELECT 
     COUNT(DISTINCT customer_id) AS unique_customers, category
@@ -134,17 +146,18 @@ FROM
 GROUP BY category
 ORDER BY unique_customers DESC;
 
+
 -- Q.10: Classify sales into Morning, Afternoon, and Evening shifts and count number of orders
 SELECT 
     shift, number_of_orders
 FROM
     (SELECT 
         CASE
-			WHEN HOUR(sale_time) <= 12 THEN 'Morning'
-			WHEN HOUR(sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
-			ELSE 'Evening'
-		END AS shift,
-		COUNT(*) AS number_of_orders
+		WHEN HOUR(sale_time) <= 12 THEN 'Morning'
+		WHEN HOUR(sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+		ELSE 'Evening'
+	END AS shift,
+	COUNT(*) AS number_of_orders
     FROM
         retail_sales
     GROUP BY shift) AS shift_data
